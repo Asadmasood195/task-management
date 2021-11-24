80f8e04657d7b13fcfa906b722e49df6ed61249d
usama-govsoft <robert@govsoft.us>
2021-11-24 11:27:13 +0500

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,46 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button
+      @btn-click="$emit('toggle-task')"
+      :text="showAddTask ? 'Close Task ' : 'Add Task'"
+      color="green"
+    />
+  </header>
+  <AddTask v-show="showAddTask" @add-task="addTask" />
+</template>
+
+<script>
+import Button from './Button.vue';
+import AddTask from './AddTask.vue';
+
+export default {
+  name: 'Header',
+  components: {
+    Button,
+    AddTask,
+  },
+  methods: {
+    addTask(task) {
+      this.$emit('add-task', task);
+    },
+  },
+  props: {
+    title: {
+      type: String,
+      default: 'default Title',
+    },
+    showAddTask: Boolean,
+  },
+  emits: ['add-task'],
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
